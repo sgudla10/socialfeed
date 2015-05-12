32508637e36584f491916d0e31c476b2b473743b
Adam Crabtree <dude@noderiety.com>
2015-05-12 12:05:45 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# nodejs-socialauthenticator-starter
-A starter project for a node.js 3rd-party login app.
+# Social Authenticator Demo
+
+Starter project for the Social Authenticator lab. The purpose of this lab is to implement the following Passport authentication strategies: local, LinkedIn, Facebook, Twitter, and Google.
+
+## Initial Setup
+
+1. Clone the repo: `git clone git@github.com:crabdude/social-authenticator`
+2. Install packages: `npm install`
+3. Update the database configuration in `config/database.js`
+4. Update auth keys in `config/auth.js`
+5. Run `npm start`
+6. Visit in your browser at: `http://127.0.0.1:8000`
---
+++ app/middlewares/passport.js
@@ -0,0 +1,36 @@
+let passport = require('passport')
+let nodeifyit = require('nodeifyit')
+require('songbird')
+
+function useExternalPassportStrategy(OauthStrategy, config, field) {
+  passport.use(new OauthStrategy(config,
+    nodeifyit(async (req, token, tokenSecretOrRefreshToken, account) => {
+      // 1. Load user from store
+      // 2. If req.user exists, we're authorizing (connecting an account)
+      // 2a. Ensure it's not associated with another account
+      // 2b. Link account
+      // 3. If not, we're authenticating (logging in)
+      // 3a. If user exists, we're logging in via the 3rd party account
+      // 3b. Otherwise create a user associated with the 3rd party account
+  })))
+}
+
+function configure(config) {
+  // Required for session support / persistent login sessions
+  passport.serializeUser(nodeifyit(async (user) => {
+    throw new Erro('Not implemented.')
+  }))
+
+  passport.deserializeUser(nodeifyit(async (user) => {
+    throw new Erro('Not implemented.')
+  }))
+
+  // useExternalPassportStrategy(LinkedInStrategy, {...}, 'linkedin')
+  // useExternalPassportStrategy(LinkedInStrategy, {...}, 'facebook')
+  // useExternalPassportStrategy(LinkedInStrategy, {...}, 'google')
+  // useExternalPassportStrategy(LinkedInStrategy, {...}, 'twitter')
+  // passport.use('local-login', new LocalStrategy({...}, (req, email, password, callback) => {...}))
+  // passport.use('local-signup', new LocalStrategy({...}, (req, email, password, callback) => {...}))
+}
+
+module.exports = {passport, configure}
